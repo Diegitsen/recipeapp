114f1ad9fc28e5a1a0cac35b655224451db46ddb
diegitsen <diegitsen@gmail.com>
2019-10-31 18:34:26 +0100

    auth done

---
+++ lib/login_admin/contentPage.dart
@@ -0,0 +1,31 @@
+abstract class Content{
+
+  //Future<HomePageRecipes> lista();
+  //Future<InicioPage> recetas(String id);
+  //Future<MapsPage> mapa();
+  //Future<ListMyrecipe> myrecipe(String id);
+  //Future<InicioPage> admin();
+
+}
+
+class contentPage implements Content{
+
+  /*Future<HomePageRecipes> lista() async{
+    return HomePageRecipes();
+  }
+
+  Future<MapsPage> mapa() async{
+    return MapsPage();
+  }
+
+  Future<InicioPage> recetas(String id) async{
+    print('en content page $id');
+    return InicioPage(id:id);
+  }
+
+  Future<ListMyrecipe> myrecipe(String id) async{
+    print('listados mis recetas $id');
+    return ListMyrecipe(id:id);
+  }*/
+
+}
